814f03a5fb339fa8f066fee681871ac4d088013f
gislab-augsburg <57507078+gislab-augsburg@users.noreply.github.com>
2023-06-15 15:58:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World, this is me, check it out now! The Funk Soul Brother. Check Commit, Check connection"
+def hello2():
     return "Hello again"
 
+def dbconn():
     #establishing the connection
     conn = psycopg2.connect(
     database="postgres", user='postgres', password='postgres', host='dzckank001.srv.muenchen.de', port= '5432'
@@ -16,7 +18,6 @@ def hello():
     #Creating a cursor object using the cursor() method
     cursor = conn.cursor()
     
-    return "Hello a third time"
 
     #Executing an MYSQL function using the execute() method
     cursor.execute("SELECT schemaname FROM pg_catalog.pg_tables WHERE tablename = 'pg_statistic';")
@@ -24,11 +25,13 @@ def hello():
     # Fetch a single row using fetchone() method.
     data = cursor.fetchone()
     
-    return "fetched: "
-    return data
-
     #Closing the connection
     conn.close()
+    
+    #return "fetched: "
+    return data
+
+
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
