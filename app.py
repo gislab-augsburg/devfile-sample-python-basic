ea0c7a391cef02cc30a31854589a75447cda234a
gislab-augsburg <57507078+gislab-augsburg@users.noreply.github.com>
2023-06-15 13:10:54 +0200

    Update app.py for postgres connection test

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask
 import os
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World, this is me, check it out now! The Funk Soul Brother. Check Commit"
+
+    #establishing the connection
+    conn = psycopg2.connect(
+    database="postgres", user='postgres', password='postgres', host='dzckank001.srv.muenchen.de', port= '5432'
+    )
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    #Executing an MYSQL function using the execute() method
+    cursor.execute("SELECT schemaname FROM pg_catalog.pg_tables WHERE tablename = 'pg_statistic'";)
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print("fetched: ",data)
+
+    #Closing the connection
+    conn.close()
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
